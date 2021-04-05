85de7b775a21339e79c8271a9a80ad20ba917ee8
lalebdi <loai.alebdi@gmail.com>
2021-04-05 13:22:04 -0400

    new pages layout

---
+++ src/pages/ProductDetails.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <router-view/>
+        <ProductDetails />
+    </div>
+</template>
+
+<script>
+import ProductDetails from '../components/ProductDetails'
+export default {
+name: 'ProductDetails',
+components: ProductDetails
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/ProductsList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+            <router-view/>
+    <SearchProduct />
+    <FilterProducts/>
+    <br />
+    <Products />
+    
+    </div>
+</template>
+
+<script>
+import Products from '../components/Products';
+import SearchProduct from '../components/SearchProduct';
+import FilterProducts from "../components/FilterProducts";
+export default {
+name: 'Products',
+components: {
+    Products,
+    SearchProduct,
+    FilterProducts,
+}
+}
+</script>
+
+<style>
+
+</style>
